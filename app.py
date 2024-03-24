cf1002a5de738582553ec4241fc6c52c6c26313e
- <46890315+s1099@users.noreply.github.com>
2024-03-24 20:11:19 +0530

    Use sidebar for params

--- app.py
+++ app.py
@@ -18,9 +18,9 @@ model = genai.GenerativeModel("gemini-pro", generation_config=generation_config)
 
 st.title("Poem Generator")
 
-title = st.text_area("Enter title for the poem: ", height=50)
+title = st.sidebar.text_area("Enter title for the poem: ", height=50)
 
-num_lines = st.slider("Approx number of lines: ", 0, 50, value=10)
+num_lines = st.sidebar.slider("Approx number of lines: ", 0, 50, value=10)
 
 tone_options = [
     "Nostalgic",
@@ -33,18 +33,18 @@ tone_options = [
     "Hopeful",
     "Serene",
 ]
-tone = st.selectbox("Select the tone or mood of the poem:", options=tone_options)
+tone = st.sidebar.selectbox("Select the tone or mood of the poem:", options=tone_options)
 if tone == "Other":
-    tone = st.text_input("Enter the tone or mood of the poem: ")
+    tone = st.sidebar.text_input("Enter the tone or mood of the poem: ")
 
 style_options = ["Free Verse", "Sonnet", "Haiku", "Other"]
-style = st.radio("Style or form:", style_options)
+style = st.sidebar.radio("Style or form:", style_options)
 if style == "Other":
-    style = st.text_input("Enter the style or form of the poem: ")
+    style = st.sidebar.text_input("Enter the style or form of the poem: ")
 
 prompt = f"Write a {style} poem titled {title} in approximately {num_lines} lines in {style} style or form"
 
-if st.button("Generate Poem"):
+if st.sidebar.button("Generate Poem"):
     try:
         chat = model.start_chat()
         response = chat.send_message(prompt)
